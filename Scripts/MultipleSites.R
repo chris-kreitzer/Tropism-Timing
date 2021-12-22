cf5079f78a1e1943d8b279eed896ce1967062cda
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-12-22 12:40:28 +0100

    update

--- Scripts/MultipleSites.R
+++ Scripts/MultipleSites.R
@@ -24,7 +24,7 @@ data_PP = data_prostate[which(data_prostate$sample_type == 'Primary'), ]
 data_PP_death = data_PP[which(data_PP$os_status == 'dead'), ]
 
 # sites
-ncols_max = max(stringr::str_count(sites_PP$met_event_age, '\\/'), na.rm = T) + 1
+ncols_max = max(stringr::str_count(data_PP_death$met_event_age, '\\/'), na.rm = T) + 1
 sites_PP = data_PP_death[, c('met_site_mapped', 'met_event_age')]
 column_names = paste0("Time", 1:ncols_max)
 
@@ -33,53 +33,15 @@ data_pp_split = tidyr::separate(sites_PP,
              col = met_event_age, 
              sep = '\\/', 
              into = column_names,
-             remove = F)
-
-View(data_pp_split)
+             remove = F, convert = T)
 
 
-df %>%
-  as_tibble() %>%
-  mutate(dup = pmap_dbl(list(V1, V2, V3), ~ n_distinct(c(...)))) %>%
-  filter(dup == 3) %>%
-  select(-dup)
-
-x = data_pp_split %>% 
-  as_tibble() %>%
-  mutate(dup = pmap_dbl(list(Time1, Time2, Time3), ~ n_distinct(c(...))))
-View(x)
-
-
-
-head(data_pp_split)
-dup_rows = apply(data_pp_split[, 3:ncol(data_pp_split)], 1, FUN = function(x) ifelse(max(table(x, na.rm = TRUE)) > 1 ,TRUE, FALSE))
-u = data_pp_split[1, 3:ncol(data_pp_split)]
-apply(u, 1, FUN = function(x) ifelse(max(table(x)) > 1, TRUE, FALSE))
-View(u)
-
-
-
-
-
-x = data.frame(one = c('a', 'a', 'x'),
-               two = c('a', 'a', 'c'),
-               third = c('a', 'b', 'y'))
-
-x = apply(x, 1, FUN = function(x) table(x))
-u = data.table::rbindlist(x)
-
-j = do.call(rbind.data.frame, x)
-
-
-
-
-
-
-
-
-
-
-
-
-
+#' how many sites are detected at the first inspection (date)
+data_pp_split = as.data.frame(data_pp_split)
+for(i in 3:length(data_pp_split)){
+  data_pp_split[, i] = as.integer(data_pp_split[, i])
+}
+data_pp_split[is.na(data_pp_split)] = 0
+data_pp_split = data_pp_split[which(data_pp_split$Time1 != 0), ]
+data_pp_split$First = rowSums(data_pp_split[, c(3:ncol(data_pp_split))] == data_pp_split[, 3], na.rm = T)
 
