cd3ceedbef6a1032d1c707ec921b466c547deef0
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-01-04 19:52:06 +0100

    new script

---
+++ Scripts/PlackettLuce.R
@@ -0,0 +1,13 @@
+## Introduction of the PlackettLuce Model
+## 
+## 01/04/2022
+## chris-kreitzer
+
+
+
+install.packages('PlackettLuce')
+library(PlackettLuce)
+
+pudding = PlackettLuce::pudding
+
+
