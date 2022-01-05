9d0ab9dbda855bacc2900a8d79609ffabe2d1cdd
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-01-05 12:33:39 +0100

    new lines

--- Scripts/PlackettLuce.R
+++ Scripts/PlackettLuce.R
@@ -10,4 +10,13 @@ library(PlackettLuce)
 
 pudding = PlackettLuce::pudding
 
+i_wins <- data.frame(Winner = pudding$i, Loser = pudding$j)
+j_wins <- data.frame(Winner = pudding$j, Loser = pudding$i)
 
+ties <- data.frame(Winner = asplit(pudding[c("i", "j")], 1),
+                   Loser = rep(NA, 15))
+R <- as.rankings(rbind(i_wins, j_wins, ties),
+                 input = "orderings")
+
+str(R)
+head(unclass(R), 2)
