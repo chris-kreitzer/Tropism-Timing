e08f51a00a0671ef33f8d1184232f933012eedfb
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-01-07 19:42:28 +0100

    metCount

--- Scripts/PlackettLuce.R
+++ Scripts/PlackettLuce.R
@@ -98,59 +98,47 @@ plot(qv, xlab = NULL, ylab = "Ability (log)", main = NULL,
 axis(1, at = seq_len(11), labels = rownames(qv$qvframe), las = 2, cex.axis = 0.6)
 
 
-
+###############################################################################
+###############################################################################
 ## PanCancer: Starting with Francisco's Table
 data = read_excel('Data/tableS2_data.xlsx', skip = 2)
 data = data[, c('cancer_type', 'sample_type', 'met_site_mapped', 'met_event_age')]
 
 #' split the data
 ncols_max = max(stringr::str_count(data$met_event_age, '\\/'), na.rm = T) + 1
-column_names = paste0('Time', 1:ncols_max)
+column_names = paste0('Rank', 1:ncols_max)
 data_split = tidyr::separate(data = data,
                              col = 'met_site_mapped',
                              sep = '\\/',
                              into = column_names,
                              remove = F,
                              convert = T)
-View(data_split)
 
-# starting with Prostate first
-data_prostate = data[which(data$cancer_type == 'prostate cancer'), ]
-data_PP = data_prostate[which(data_prostate$sample_type == 'Primary'), ]
-data_PP_death = data_PP[which(data_PP$os_status == 'dead'), ]
+ncols_time = max(stringr::str_count(data_split$met_event_age, '\\/'), na.rm = T) + 1
+column_times = paste0('Time', 1:ncols_time)
+data_split = tidyr::separate(data = data_split,
+                             col = 'met_event_age',
+                             sep = '\\/',
+                             into = column_times,
+                             remove = F,
+                             convert = T)
 
-# sites
-ncols_max = max(stringr::str_count(data_PP_death$met_event_age, '\\/'), na.rm = T) + 1
-sites_PP = data_PP_death[, c('met_site_mapped', 'met_event_age')]
-column_names = paste0("Time", 1:ncols_max)
+#' modies
+data_split = data_split[!is.na(data_split$met_site_mapped), ]
+data_split = as.data.frame(data_split)
+for(i in 40:ncol(data_split)){
+  data_split[, i] = as.numeric(as.integer(data_split[, i]))
+}
 
-#' split the dataframe
-data_pp_split = tidyr::separate(sites_PP, 
-                                col = met_event_age, 
-                                sep = '\\/', 
-                                into = column_names,
-                                remove = F, convert = T)
+#' how many sites are we seeing; how is the frequency of these sites
+unique_sites = as.character(unique(unlist(data_split[, 4:38])))
+data_split$metCount = rowSums(!is.na(data_split[, 4:38]))
 
-
-
-
-
-
-
-
-
-
-adjacency(dummy.ranking)
-
-summary(mod, ref = NULL)
-
-
-
-
-
-
-
-
+barplot(sort(data_split$metCount))
+abline(h = quantile(data_split$metCount, probs = 0.9),
+       col = 'red',
+       lwd = 2,
+       lty = 'dashed')
 
 
 
