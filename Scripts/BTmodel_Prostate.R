deb93619dc27c12819f748e5d60a3c1ea92c384b
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-01-10 11:11:26 +0100

    PL.PR

--- Scripts/BTmodel_Prostate.R
+++ Scripts/BTmodel_Prostate.R
@@ -1,4 +1,5 @@
-## Tour through Prostate Cancer
+## Tour through Prostate Cancer;
+## We start with the BradleyTerry Model and move on to the PlackettLuce model
 ## 
 ## 01/09/2022
 ## chris-kreitzer
@@ -69,6 +70,125 @@ par(mar = c(8, 4,4,3))
 barplot(sort(table(Prostate$Rank1), decreasing = T), las = 2, cex.names = 0.65)
 barplot(sort(table(Prostate$Rank2), decreasing = T), las = 2, cex.names = 0.65)
 barplot(sort(table(Prostate$Rank3), decreasing = T), las = 2, cex.names = 0.65)
+
+
+########
+#' modify the data for the BT model:
+unique.sites = length(unique(Prostate$Rank1))
+sites_names = unique(Prostate$Rank1)
+occurrence = table(Prostate$Rank1)
+
+#' modify the table
+m = matrix(nrow = unique.sites, 
+           ncol = unique.sites, 
+           dimnames = list(sites_names, sites_names))
+m = as.data.frame(m)
+
+for(i in 1:nrow(m)){
+  m[row.names(m)[i], ] = occurrence[which(names(occurrence) == row.names(m)[i])]
+}
+
+m = as.table(as.matrix(m))
+
+#' counts to binomial
+c.binom = BradleyTerry2::countsToBinomial(m)
+
+pp.model = BradleyTerry2::BTm(cbind(win1, win2),
+                              player1, player2,
+                              ~player, id = 'player',
+                              data = c.binom)
+#' update the model
+pp.model.update = update(pp.model, refcat = 'breast')
+summary(pp.model.update)
+
+#' setting the 'weakest' as reference. In this case we are using liver;
+#' occurred just once
+
+#' now lets extract the model estimates and make a plot
+plot.pp = as.data.frame(coefficients(pp.model.update))
+plot.pp$sig = coef(summary(pp.model.update))[,4]
+plot.pp$variable = row.names(plot.pp)
+plot.pp$variable = substr(plot.pp$variable, start = 7, nchar(plot.pp$variable))
+colnames(plot.pp)[1] = 'estimate'
+
+ggplot(plot.pp, aes(x = reorder(variable, estimate), y = estimate)) +
+  geom_point(col = ifelse(plot.pp$sig < 0.05, 'red', 'blue')) +
+  coord_flip() +
+  scale_y_continuous(
+    sec.axis = dup_axis()) +
+  geom_hline(yintercept = 1, linetype = 'dashed') +
+  labs(x = '', y = 'BT model estimates')
+
+
+#' Intervals based on quasi standard errors
+pp.model.qv = qvcalc(BTabilities(pp.model.update))
+plot(pp.model.qv, las = 2, cex.axis = 0.65)
+abline(h = 1)
+
+
+#' ML estimation
+ML_Prostate = BT_ML(m)
+#' this convergence actually to the same as the simply frequency of occurrence
+
+
+###############################################################################
+###############################################################################
+#' PlackettLuce model
+Prostate = Prostate[, 4:ncol(Prostate)]
+
+#' delete columns with no entry
+for(i in 4:26){
+  print(i)
+  if(all(is.na(Prostate[, i]))){
+    Prostate[, i] = NULL
+  }
+}
+Prostate[,25:ncol(Prostate)] = NULL
+Prostate[,13:ncol(Prostate)] = NULL
+
+#' unique sites
+unique_sites_Prostate = as.character(unique(unlist(Prostate[, 1:ncol(Prostate)])))
+unique_sites_Prostate = unique_sites_Prostate[!is.na(unique_sites_Prostate)]
+names(unique_sites_Prostate) = seq(1, length(unique(unique_sites_Prostate)), 1)
+
+#' recode the named characters into numeric values
+Prostate[Prostate == "regional_lymph"] = 1L
+Prostate[Prostate == "genital_male"] = 4L
+Prostate[Prostate == "lung"] = 7L
+Prostate[Prostate == "peritoneum"] = 10L
+Prostate[Prostate == "pleura"] = 13L
+Prostate[Prostate == "adrenal_gland"] = 16L
+Prostate[Prostate == "breast"] = 19L
+Prostate[Prostate == "mediastinum"] = 20L
+Prostate[Prostate == "other"] = 2L
+Prostate[Prostate == "dist_lymph"] = 5L
+Prostate[Prostate == "liver"] = 8L
+Prostate[Prostate == "bowel"] = 11L
+Prostate[Prostate == "kidney"] = 14L
+Prostate[Prostate == "skin"] = 17L
+Prostate[Prostate == "head_and_neck"] = 21L
+Prostate[Prostate == "bone"] = 3L
+Prostate[Prostate == "bladder_or_urinary_tract"] = 6L
+Prostate[Prostate == "lymph"] = 9L
+Prostate[Prostate == "cns_brain"] = 12L
+Prostate[Prostate == "peripheral_nervous_system"] = 15L
+Prostate[Prostate == "biliary_tract"] = 18L
+
+#' convert
+x_matrix = as.matrix(Prostate, rownames.force = F)
+mode(x_matrix) = 'numeric'
+attr(x_matrix, which = 'sites') = unique_sites_Prostate
+
+
+
+
+
+
+
+
+
+
+
 
 
 
